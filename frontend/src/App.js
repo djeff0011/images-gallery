b8a59b791bb5b433caf9dda63e7d3a8af27e71ac
Donahue <donahuejeffers@hotmail.com>
2022-05-04 16:38:43 -0400

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+/*
+function App() {
+  return (
+    <div className="App">
+      <h1>Images Gallery</h1>
+    </div>
+  );
+}*/
+
+/* Arrow function instead of using the keyword function */
+const App = () => {
+  return (
+    <div>
+      <h1>Images Gallery</h1>
+    </div>
+  );
+}
+
+export default App;
