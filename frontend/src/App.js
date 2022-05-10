65f4929f5179f17d16f01e6b8f025f23a4e7936f
Donahue <donahuejeffers@hotmail.com>
2022-05-10 13:01:41 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react'; /* useState is a react hook(basically a function) we will use to deal with the state of the user input*/
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Search from './components/Search';
 
 /* Arrow function instead of using the keyword function */
 const App = () => {
+  const [word, setWord] = useState('') /* word hold data and setWord function updates word. useState returns two elements in an array so we destruct it here*/
+
+  /* This handles the submission of the search button in the search component*/
+  /* below e.target[0].value retrieves the value for the text in the search bar */
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    console.log(word)
+  };
+
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/> 
     </div>
   );
 }
 
 export default App;
+
