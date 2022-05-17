d404ca14e208597fb637114427518b59b0e4cfed
Donahue <donahuejeffers@hotmail.com>
2022-05-17 13:35:01 -0400

    First api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
+
+
 /* Arrow function instead of using the keyword function */
 const App = () => {
   const [word, setWord] = useState('') /* word hold data and setWord function updates word. useState returns two elements in an array so we destruct it here*/
@@ -12,7 +16,18 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault()
     console.log(word)
-  };
+    /* Use fetch to make the api call. Like request in python. Use ${} to use local variables instead
+       of using the + to concat to the string like in python. Mouse over the fetch keyword to see what
+       it returns. It returns a Promise, which is a response */
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   return (
     <div>
