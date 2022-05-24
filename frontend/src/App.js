7f1720b01e482b952e056b3606d1229dafda1277
Donahue <donahuejeffers@hotmail.com>
2022-05-24 16:25:28 -0400

    eslint and prittier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,42 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
-
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 /* Arrow function instead of using the keyword function */
 const App = () => {
-  const [word, setWord] = useState('') /* word hold data and setWord function updates word. useState returns two elements in an array so we destruct it here*/
+  const [word, setWord] =
+    useState(
+      ''
+    ); /* word hold data and setWord function updates word. useState returns two elements in an array so we destruct it here*/
 
   /* This handles the submission of the search button in the search component*/
   /* below e.target[0].value retrieves the value for the text in the search bar */
   const handleSearchSubmit = (e) => {
-    e.preventDefault()
-    console.log(word)
+    e.preventDefault();
+    console.log(word);
     /* Use fetch to make the api call. Like request in python. Use ${} to use local variables instead
        of using the + to concat to the string like in python. Mouse over the fetch keyword to see what
        it returns. It returns a Promise, which is a response */
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((result) => result.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-      setWord(''); //set the input box on the search to empty or default. clears it
-
-  }
+      });
+    setWord(''); //set the input box on the search to empty or default. clears it
+  };
 
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />  
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
-    
   );
-}
+};
 
 export default App;
-
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,30 +1,32 @@
-/* eslint-disable react/jsx-no-undef */
 import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const Search = ({word, setWord, handleSubmit}) => {
-    return (
-        <Container className="mt-4">
-            <Row className="justify-content-center">
-                <Col xs={12} md={8}>
-                    <Form onSubmit={handleSubmit}>
-                        <Row>
-                            <Col xs={15}>
-                                <Form.Control
-                                 type="text"
-                                 value={word}
-                                 onChange={(e) => setWord(e.target.value)}                 
-                                 placeholder="Search for new image..." />                              
-                            </Col>
-                            <Col xs={3}>
-                                <Button variant="primary" type="submit">Search</Button>
-                            </Col>
-                        </Row>                              
-                    </Form>
-                </Col>  
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={15}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..."
+                />
+              </Col>
+              <Col xs={3}>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
             </Row>
-        </Container>
-    );
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
