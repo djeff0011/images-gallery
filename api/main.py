4ee920913e7ef229994dec5903b782cc276ede2d
Donahue <donahuejeffers@hotmail.com>
2022-06-06 17:26:34 -0400

    Enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -1,25 +1,34 @@
-import os   # Need to import this so we can access the local environment variables. Os used for much more as well
-from flask import Flask, request # This request is for the flask app used to get access to the client request that is sent to flask app from other clients
-import requests     # This request is from python itself. The one I used to create client requests and send them to another server
-from dotenv import load_dotenv # Import this so we can get the local env file we created (.env.local in the api directory)
-from flask_cors import CORS  # Cross-Origin Resource Sharing. Needed to access our api being ran on the same ip 127.0.0.1 as our frontend app
+import os   # Need to import this so we can access the local environment variables.
+# This request used to get access to the client request
+from flask import Flask, request
+# This one I used to create client requests and send them to another server
+import requests
+# To access the local env file we created (.env.local in the api directory)  
+from dotenv import load_dotenv
+# Cross-Origin Resource Sharing. Needed to access our api
+# being ran on the same ip 127.0.0.1 as our frontend app
+from flask_cors import CORS  
 
-load_dotenv(dotenv_path="./.env.local") # Here we set the path to the file it self
 
-UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPALSH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+load_dotenv(dotenv_path="./.env.local")  # Here we set the path to the file it self
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPALSH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 # To disable debug mode put this DEBUG="" in the .env.local file within the api folder
 
-# make sure we have api key or raise an error that will stop the program from running till we put the key into the .env.local file 
+# make sure we have api key or raise an error that will stop the program from running
+#  till we put the key into the .env.local file
 if not UNSPALSH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert the UNSPLASH_KEY there")
+    raise EnvironmentError("Please create .env.local file and insert the UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
 
-#Not good to set debug mode when in production. This in not production but not a good practice to have it here.
-app.config["DEBUG"] = DEBUG  # This enables debug mode in out flask app. *** THIS WILL AUTO RESTART OUR APP after every change made in it
+# Not good to set debug mode when in production.
+# *** THIS WILL AUTO RESTART OUR APP after every change made in it
+app.config["DEBUG"] = DEBUG  # This enables debug mode in out flask app. 
+
 
 @app.route("/new-image")
 def new_image():
@@ -34,6 +43,8 @@ def new_image():
     data = (response.json())
     return data
 
-# This is how you run the flask application within the module. (A module is any .py file)
+# This is how you run the flask application within the module. (A module is any .py)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050) # using the .run method of the app you can run the flask server
+    app.run(host="0.0.0.0", port=5050)  # using the .run runs the flask server
