61172dddf75323f9b31e225b6193071439cb6ead
Donahue <donahuejeffers@hotmail.com>
2022-07-06 13:39:52 -0400

    Introduce axios and rewrite new-image fetch call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'; /* useState is a react hook(basically a function) we will use to deal with the state of the user input*/
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
@@ -23,25 +24,33 @@ const App = () => {
 
   /* This handles the submission of the search button in the search component*/
   /* below e.target[0].value retrieves the value for the text in the search bar */
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
 
+    // fetch does not check if we succeeded in an operation before moving on, wo we use axios below
     /* Use fetch to make the api call. Like request in python. Use ${} to use local variables instead
        of using the + to concat to the string like in python. Mouse over the fetch keyword to see what
        it returns. It returns a Promise, which is a response */
-    fetch(
-      `${API_URL}/new-image?query=${word}`
-    )
-      .then((result) => result.json())
-      .then((data) => {
-        setImages([
-          { ...data, title: word },
-          ...images,
-        ]); /* the ... is a javascript spread opperator to pull data from another array. data is the current images searched for*/
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // fetch(
+    //   `${API_URL}/new-image?query=${word}`
+    // )
+    //   .then((result) => result.json())
+    //   .then((data) => {
+    //     setImages([
+    //       { ...data, title: word },
+    //       ...images,
+    //     ]); /* the ... is a javascript spread opperator to pull data from another array. data is the current images searched for*/
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`)
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error)    //not good to have console logs in code. can do a popup msg or something to warn about errors
+    }
     setWord(''); //set the input box on the search to empty or default. clears it
   };
   const handleDeleteImage = (id) => {
